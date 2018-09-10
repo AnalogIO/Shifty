54827c86461d670f42b6d4c308a99d7b3680324e
Frederik Dam Christensen <fdc@shape.dk>
2018-09-10 11:20:28 +0200

    fixed untracked files

--- ShiftPlanningAPI/Sources/Config.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-struct Config {
-    #if DEBUG
-    static let apikey: String = "INSERT TEST-ENV API KEY"
-    #else
-    static let apikey: String = "INSERT PROD-ENV API KEY"
-    #endif
-}
